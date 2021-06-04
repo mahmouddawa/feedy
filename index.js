33c8bfd7254f4a2812e25a9ede403861b22da08e
Mahmoud Dawa <mahmouddawa990@gmail.com>
2021-06-04 18:23:39 +0200

    saving data to Mongodb
    building the User Model and saving the data to the MongoDB cluster

--- index.js
+++ index.js
@@ -1,35 +1,24 @@
 const express = require('express');
-const passport = require('passport');
-const GoolgeStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
-
-
+require('./models/User');
+require('./services/passport');
 
 const app = express();
 
-passport.use(new GoolgeStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL:'/auth/google/callback' 
-  }, (accessToken, refreshToken, profile, done) =>{
-    console.log('access token: ',accessToken);
-    console.log('refresh token: ', refreshToken);
-    console.log('profile: ', profile);
-  })
-);
+mongoose.connect(keys.mongoURI, {
+  // to solve some issue with deployment on Heroku
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
-console.log('somethig');
-
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT)
 
+console.log('running on Port : ', PORT);
+app.listen(PORT);
 
 
 
---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+//const Schema = mongoose.Schema;
+const { Schema }= mongoose;
+
+const userSchema = new Schema({
+  googleId: String
+});
+
+
+mongoose.model('users', userSchema);
+
+
+// in testing environment your Model might be required multiple times and this will let Mongoose think
+// that we are trying to require multiple models called 'users' => error 
+// 
