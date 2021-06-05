093719aa366d93b6b1550ef659d0ae0ff307c5d6
Mahmoud Dawa <mahmouddawa990@gmail.com>
2021-06-05 18:48:47 +0200

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -6,9 +6,5 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY
 }
 
-// googleClientID : '72321471724-3srp2a547v74m7pf6mvqah3s9fpd7err.apps.googleusercontent.com',
-// googleClientSecret: 'DgkUJ-Wm7XqE9MKeb7cpJKD3',
-// mongoURI: 'mongodb+srv://Admin:7neq4OQz5aZrYATg@cluster0.rfl5m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-// cookieKey: 'laskdjflaweiflkasdjfo2234lasd'
 
 
