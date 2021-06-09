3a15e6a36c4d6c94920c0e69afe19a2c111fe15b
Mahmoud Dawa <mahmouddawa990@gmail.com>
2021-06-09 18:31:18 +0200

    adjustenments for Heroku deployments

--- index.js
+++ index.js
@@ -31,6 +31,16 @@ mongoose.connect(keys.mongoURI, {
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // express serve up production assest like  main.js and main.css
+  app.use(express.static("client/build"));
+  // express serve up the index.html file if it doesn't find the rout
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000; 
 
 console.log("running on Port : ", PORT);
